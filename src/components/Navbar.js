cd0901a5e03c0917f2fa56b2ed7c3625144c778f
varamara <mikahvaarama@gmail.com>
2023-12-18 12:36:58 +0100

    started proj

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Portfoio</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">about</a>
+                <a href="/projects">projects</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
